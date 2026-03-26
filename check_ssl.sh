7e1c8b4cc28dedc8cd807dc31215fa2f45dd9e11
gabrielglusgold-commits <gabrielglusgold@gmail.com>
2026-03-26 12:51:59 -0300

    script

---
+++ check_ssl.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DOMAIN="aseconecta.com.ar"
+DNS_SERVER="8.8.8.8"
+
+echo "[+] Descubriendo subdominios desde crt.sh..."
+
+curl -s "https://crt.sh/?q=%25.$DOMAIN&output=json" \
+| jq -r '.[].name_value' \
+| tr '\n' ',' | tr ',' '\n' \
+| sort -u > subdominios.txt
+
+echo "[+] Analizando certificados..."
+
+while read host; do
+  IP=$(dig @$DNS_SERVER $host +short | head -n1)
+
+  if [[ -z "$IP" ]]; then
+    echo "[!] $host -> no resuelve"
+    continue
+  fi
+
+  CERT_INFO=$(echo | openssl s_client -connect $IP:443 -servername $host 2>/dev/null \
+    | openssl x509 -noout -subject -issuer -enddate 2>/dev/null)
+
+  if [[ -n "$CERT_INFO" ]]; then
+    echo "[$host][$IP]"
+    echo "$CERT_INFO"
+    echo "----------------------------------"
+  else
+    echo "[!] $host -> sin SSL o fallo"
+  fi
+
+done < subdominios.txt
